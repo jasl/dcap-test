e6892cad03ca0e39d9934ed7eac1a595712bf5ff
jasl <jasl9187@hotmail.com>
2022-07-29 03:28:50 +0800

    First commit

---
+++ src/main.rs
@@ -0,0 +1,13 @@
+use anyhow::Result;
+use std::fs;
+
+fn create_quote_vec(data: &[u8]) -> Result<Vec<u8>> {
+    // TODO: DCAP-only for now, should determine which RA type via `/dev/attestation/attestation_type`
+    fs::write("/dev/attestation/user_report_data", data)?;
+    Ok(fs::read("/dev/attestation/quote")?)
+}
+
+fn main() {
+    let quote = create_quote_vec("Hello, world!".as_bytes()).expect("Create quote error");
+    println!("0x{}", hex::encode(quote));
+}
